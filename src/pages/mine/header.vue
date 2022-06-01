155209f0d60caa0b3b52c64430c19e81f372e687
snowingfox <1503401882@qq.com>
2022-06-02 00:28:45 +0800

    :sparkles: 更新提示语

--- src/pages/mine/header.vue
+++ src/pages/mine/header.vue
@@ -11,11 +11,13 @@ const greetingWords = computed(() => {
 
   const qaq = getRandomQAQ('happy')[0]
 
-  return dayPeriod < 12
-    ? ['早上好呀,', `今天又是充满希望的一天${qaq}`]
-    : dayPeriod < 18
-      ? ['下午好', `快来喝一杯下午茶吧${qaq}`]
-      : ['晚上好', '不要熬夜到太晚哦٩(ˊ〇ˋ*)و']
+  return dayPeriod < 6
+    ? ['凌晨了,', `同学记得要休息噢，${qaq}`]
+    : dayPeriod > 6
+      ? ['早上好呀,', `今天又是充满希望的一天${qaq}`]
+      : dayPeriod < 18
+        ? ['下午好', `快来喝一杯下午茶吧${qaq}`]
+        : ['晚上好', '不要熬夜到太晚哦٩(ˊ〇ˋ*)و']
 })
 </script>
 
