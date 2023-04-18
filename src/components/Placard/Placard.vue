d4a790dfd24193959a7ecb8c9355b8a3e6e7a310
yixiaojiu <1918418506@qq.com>
2023-04-18 13:46:07 +0800

    style: :art: 更改公告样式

--- src/components/Placard/Placard.vue
+++ src/components/Placard/Placard.vue
@@ -20,11 +20,11 @@ const handleClick = () => {
 
 <template>
   <div v-if="show && message" class="fixed w-full h-full left-0 top-0 z-[1100] bg-black/70 flex items-center justify-center">
-    <div class="relative w-2/3 px-4 py-6 rounded-xl bg-white">
-      <div class="absolute p-1 top-0 right-0" @click="handleClick">
+    <div class="box-border relative w-4/5 px-8 pt-8 pb-10 rounded-xl bg-white">
+      <div class="absolute p-1 top-2 right-2" @click="handleClick">
         <van-icon name="cross" size="20px" color="#c8c9cc" />
       </div>
-      <h3 class="text-center text-lg mb-1">
+      <h3 class="text-center text-lg mb-3">
         {{ title }}
       </h3>
       <p class="text-sm text-slate-500/80">
